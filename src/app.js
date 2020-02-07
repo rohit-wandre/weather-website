6401714b7c9ee3259c5501d9ac39ca4a5cd23ca5
unknown <rohit.wandre@yahoo.com>
2020-02-07 09:52:09 +0530

    Remove uncessary console.log() call

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-
 const app = express()
 
 // Define paths for Express config
